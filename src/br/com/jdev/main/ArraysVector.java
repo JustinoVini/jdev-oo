04de40f5fc8fe6b645ee926f8321aece8625b61e
Vinicius Justino <visilva5690@hotmail.com>
2023-07-25 21:06:32 -0300

    criando e inserindo dados no array

--- src/br/com/jdev/main/ArraysVector.java
+++ src/br/com/jdev/main/ArraysVector.java
@@ -1,5 +1,22 @@
 package br.com.jdev.main;
 
 public class ArraysVector {
-
+	
+	public static void main(String[] args) {
+		
+		/*Array pode ser de todos os tipos...*/
+		
+		/*sempre deve ter uma quantidade de posições definidas*/
+		double notas[] = new double[4];
+		
+		/*Inserindo valores nos arrays*/
+		notas[0] = 9.8;
+		notas[1] = 7.8;
+		notas[2] = 9.8;
+		notas[3] = 9.8;
+		
+		System.out.println(notas);
+		
+	}
+	
 }
