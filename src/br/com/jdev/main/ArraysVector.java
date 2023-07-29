2f454a1f43bb1d8cc84c3fc7e8dd2db607e7c0e8
Vinicius Justino <visilva5690@hotmail.com>
2023-07-29 17:14:24 -0300

    criando arrays com dinamismo

--- src/br/com/jdev/main/ArraysVector.java
+++ src/br/com/jdev/main/ArraysVector.java
@@ -1,22 +1,30 @@
 package br.com.jdev.main;
 
+import javax.swing.JOptionPane;
+
 public class ArraysVector {
-	
+
 	public static void main(String[] args) {
+
+		String posicoes = JOptionPane.showInputDialog("Quantas posições o array deve ter?");
+
+		/* Array pode ser de todos os tipos... */
+		/* sempre deve ter uma quantidade de posições definidas */
+		double notas[] = new double[Integer.parseInt(posicoes)]; 
+		/* passando o valor de posições do array na entrada de
+		dados*/
+
+		/*Para cada posicao solicitar um for*/
+		for (int pos = 0; pos < notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição?" + (pos + 1));
+			notas[pos] = Double.valueOf(valor);
+		}
 		
-		/*Array pode ser de todos os tipos...*/
-		
-		/*sempre deve ter uma quantidade de posições definidas*/
-		double notas[] = new double[4];
-		
-		/*Inserindo valores nos arrays*/
-		notas[0] = 9.8;
-		notas[1] = 7.8;
-		notas[2] = 9.8;
-		notas[3] = 9.8;
-		
-		System.out.println(notas);
-		
+		/* Percorrendo os arrays */
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Nota " + (i + 1) + " é = " + notas[i]);
+		}
+
 	}
-	
+
 }
