3d33ed3e02e7b04528574cbe32f8e91b12c4b053
Vinicius Justino <visilva5690@hotmail.com>
2023-07-30 21:52:48 -0300

    descobrindo maior e menor nota dos alunos

--- src/br/com/jdev/main/VetoresMain.java
+++ src/br/com/jdev/main/VetoresMain.java
@@ -9,6 +9,7 @@ public class VetoresMain {
 		
 		/*Passando as notas estaticamente*/
 		double[] notas = {8.8,9.8,4.5,6.8};
+		double[] notasLogica = {10.0,9.7,4.5,6.8};
 		
 		/*Chamando o aluno*/
 		Aluno aluno = new Aluno();
@@ -23,10 +24,46 @@ public class VetoresMain {
 		aluno.getDisciplinas().add(disciplina);
 		
 		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("Curso de Java");
-		disciplina2.setNota(notas);	
+		disciplina2.setDisciplina("Engenharia de software");
+		disciplina2.setNota(notasLogica);	
 		
 		aluno.getDisciplinas().add(disciplina2);
+		
+			
+		
 	}
 	
 }
+
+/*
+ * System.out.println("Nome do Aluno = " + aluno.getNome() + "inscrito no curso: " 
+		+ aluno.getNomeEscola());
+		System.out.println("============= Disciplinas do aluno =============");
+		for (Disciplina disc : aluno.getDisciplinas()) {
+			
+			System.out.println("Disciplinas : " + disc.getDisciplina());
+			System.out.println("As notas da disciplina são: ");
+			
+			double notaMaxima = 0.0;
+			double notaMinima = 0.0;
+			
+			for (int pos = 0; pos < disc.getNota().length; pos++) {
+				System.out.println("nota " + (pos + 1) + " é igual = " + disc.getNota()[pos]);
+				
+				/*Descobrindo a maior nota
+				if (pos == 0) {
+					notaMaxima = disc.getNota()[pos];
+					notaMinima = disc.getNota()[pos];
+				} else {
+					if (disc.getNota()[pos] > notaMaxima) {
+						notaMaxima = disc.getNota()[pos];
+					} else if (disc.getNota()[pos] < notaMaxima) {
+						notaMinima = disc.getNota()[pos];
+					}
+				}
+			}
+			
+			System.out.println("A maior nota da disciplina = " + disc.getDisciplina() + " é de valor : " + notaMaxima);
+			System.out.println("A menor nota da disciplina = " + disc.getDisciplina() + " é de valor : " + notaMinima);
+		}
+ * */
