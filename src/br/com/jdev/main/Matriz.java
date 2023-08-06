7b532f6988bcc1142ebd883adbb18ee9a4347e1d
Vinicius Justino <visilva5690@hotmail.com>
2023-08-06 10:17:37 -0300

    Lendo uma Matriz multidimensional

--- src/br/com/jdev/main/Matriz.java
+++ src/br/com/jdev/main/Matriz.java
@@ -13,8 +13,15 @@ public class Matriz {
 		notas[1][0] = 60;
 		notas[1][1] = 50;
 		notas[1][2] = 20;
+				
+		/*Percorrendo as linhas*/
+		for (int posLinha = 0; posLinha < notas.length; posLinha++) {			
+			/*Percorrendo as colunas*/
+			for (int posColuna = 0; posColuna < notas[posLinha].length; posColuna++) {
+				System.out.println("Valor da matriz: " + notas[posLinha][posColuna]);
+			}
+		}
 		
-		System.out.println(notas);
 	}
 	
 }
