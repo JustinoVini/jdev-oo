7486a977ea253ba727b9b2cf83eb2dc67786b89c
Vinicius Justino <visilva5690@hotmail.com>
2023-08-09 20:05:43 -0300

    Controlando a Thead 1 com Start e Stop

--- src/br/com/jdev/thread/TelaThread.java
+++ src/br/com/jdev/thread/TelaThread.java
@@ -5,72 +5,112 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 
 public class TelaThread extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 
-	private JPanel jPanel = new JPanel(new GridBagLayout()); /*Painel de componentes*/
-	
-	private JLabel descricaoHora = new JLabel("Time Thread 1"); /*Time thread 1*/
+	private JPanel jPanel = new JPanel(new GridBagLayout()); /* Painel de componentes */
+
+	private JLabel descricaoHora = new JLabel("Time Thread 1"); /* Time thread 1 */
 	private JTextField mostraTempo = new JTextField();
-	
-	private JLabel descricaoHora2 = new JLabel("Time Thread 2"); /*Time thread 1*/
+
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2"); /* Time thread 1 */
 	private JTextField mostraTempo2 = new JTextField();
-	
+
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
+
+	private Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) { /* Fica sempre rodando */
+				mostraTempo
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss")
+								.format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
 	
-	public TelaThread() { /*Vai executar o que tiver dentro da abertura da execução*/
+	private Thread thread1Time;
+
+	public TelaThread() { /* Vai executar o que tiver dentro da abertura da execução */
 		setTitle("Tela timer com thread");
 		setSize(new Dimension(240, 360));
 		setLocationRelativeTo(null);
 		setResizable(false);
-		
-		/*Inicializando o grid*/
-		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /*Layout control*/
+
+		/* Inicializando o grid */
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /* Layout control */
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
 		gridBagConstraints.gridwidth = 2;
 		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-		
+
 		descricaoHora.setPreferredSize(new Dimension(200, 25));
 		jPanel.add(descricaoHora, gridBagConstraints);
-		
+
 		mostraTempo.setPreferredSize(new Dimension(200, 25));
 		mostraTempo.setEditable(false);
-		gridBagConstraints.gridy++; /*Pulando o tamanho na lista*/
+		gridBagConstraints.gridy++; /* Pulando o tamanho na lista */
 		jPanel.add(mostraTempo, gridBagConstraints);
-		
+
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy++;
 		jPanel.add(descricaoHora2, gridBagConstraints);
-		
+
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		mostraTempo2.setEditable(false);
-		gridBagConstraints.gridy++; /*Pulando o tamanho na lista*/
+		gridBagConstraints.gridy++; /* Pulando o tamanho na lista */
 		jPanel.add(mostraTempo2, gridBagConstraints);
-		
+
 		gridBagConstraints.gridwidth = 1;
-		
+
 		jButton.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridy++; /*Pulando o tamanho na lista*/
+		gridBagConstraints.gridy++; /* Pulando o tamanho na lista */
 		jPanel.add(jButton, gridBagConstraints);
-		
+
 		jButton2.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridx++; /*Pulando o tamanho na lista*/
+		gridBagConstraints.gridx++; /* Pulando o tamanho na lista */
 		jPanel.add(jButton2, gridBagConstraints);
 		
-		add(jPanel, BorderLayout.WEST);
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { /*Executa o click do botao*/
+				thread1Time = new Thread(thread1);	
+				thread1Time.start();
+			}
+		});
 		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { /*Executa o click do botao*/
+				thread1Time.stop();
+			}
+		});
+
+		add(jPanel, BorderLayout.WEST);
+
 		setVisible(true);
 	}
 }
